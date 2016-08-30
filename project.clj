e0d2143aebbb28efaba0a94e58676040040fb2c4
Denis Korzunov <disya2@gmail.com>
2016-08-30 13:45:10 +0600

    Fix description in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject sleeping-barber "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Sleeping Barber Problem"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
